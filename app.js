20d3a98d912bd41ddf6b338a040938ca26cd4adc
Gabriel Neves <gabriel.bneves@gmail.com>
2020-10-05 14:16:29 +0100

    CRUD functions

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(post);
+//   }
+
+// });
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
-});
+   
+})
